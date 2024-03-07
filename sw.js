fa7ac1c18a8c9b106e2009554baa588bc73a9846
urasdbwd <extratoppingsplease123@gmail.com>
2024-03-06 21:48:18 -0800

    idk

--- sw.js
+++ sw.js
@@ -3,6 +3,10 @@ importScripts('/script/config.js');
 importScripts('/deps.js');
 importScripts(__uv$config.sw || '/script/sw.js');
 let sw = new UVServiceWorker();
+sw.on("request", (event) => {
+  event.data.headers["user-agent"] =
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0";
+});
     fetch(`${location.origin}${globals.injectScriptUrl}`, {cache: "no-store"})
       .then(response => {
         if (response.ok) {
